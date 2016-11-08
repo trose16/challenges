f2353c03019a089228c1be53290ec35b96fd730f
sara <sara@huhbub.com>
2016-11-08 16:48:23 +0000

    step 6

---
+++ boris_bikes/lib/boris_bikes.rb
@@ -0,0 +1,7 @@
+class DockingStation
+
+  def docking_station
+    'it releases a bike'
+  end
+
+end
---
+++ boris_bikes/spec/boris_bikes_spec.rb
@@ -0,0 +1,7 @@
+require 'boris_bikes'
+
+describe DockingStation do
+  it 'releases a bike' do
+  expect(DockingStation.new.docking_station).to eq 'it releases a bike'
+  end
+end
